72c0a75ad484ca3e50588168e8187f2fa41bdf0d
Glenn Langdon <glennlangdon@gmail.com>
2024-04-11 16:33:36 -0500

    Update demo_test copy.py

--- django_testing/demo_test copy.py
+++ django_testing/demo_test copy.py
@@ -1,42 +1,5 @@
-# from django.test import TestCase
-# from .models import Flight, Airport, Employee
-
-
-# class FlightTestCase(TestCase):
-#     def setUp(self):
-#         # Create airports
-#         a1 = Airport.objects.create(code="AAA", city="City A")
-#         a2 = Airport.objects.create(code="BBB", city="City B")
-
-#         # Create flights
-#         Flight.objects.create(origin=a1, destination=a2, duration=100)
-#         Flight.objects.create(origin=a1, destination=a1, duration=200)
-#         Flight.objects.create(origin=a1, destination=a2, duration=100)
-
-#     def test_departure_count(self):
-#         a = Airport.objects.get(code="AAA")
-#         self.assertEqual(a.departures.count(), 3)
-
-#     def test_arrivals_count(self):
-#         a = Airport.objects.get(code="AAA")
-#         self.assertEqual(a.arrivals.count(), 1)
-
-#     def test_valid_flight(self):
-#         a1 = Airport.objects.get(code="AAA")
-#         a2 = Airport.objects.get(code="BBB")
-#         f = Flight.objects.create(origin=a1, destination=a2, duration=100)
-#         self.assertTrue(f.is_valid_flight())
-
-#     def test_invalid_flight_destination(self):
-#         a1 = Airport.objects.get(code="AAA")
-#         f = Flight.objects.create(origin=a1, destination=a1, duration=100)
-
-#     def test_invalid_flight_duration(self):
-#         a1 = Airport.objects.get(code="AAA")
-#         a2 = Airport.objects.get(code="BBB")
-#         f = Flight.objects.create(origin=a1, destination=a2, duration=0)
-#         self.assertFalse(f.is_valid_flight())
-
+from django.test import TestCase
+from .models import Flight, Airport, Employee
 
 class EmployeeTestCase(TestCase):
     def setUp(self):
